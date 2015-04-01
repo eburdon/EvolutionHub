48deb9d0a5fb4d3ea5b8073653eac369caace3b5
eburdon <eburdon@uvic.ca>
2015-04-01 13:21:46 -0700

    Changed accordion body font; added repo links to questionsList and codebaseList; completed summaries for both lists

--- webApp/public/app/app.js
+++ webApp/public/app/app.js
@@ -113,36 +113,62 @@ application.controller('questionListController', function() {
 	vm.message = 'Question list stuff here!'; // DEBUG STATEMENT
 
 	// Defined in alphabetical order; projectDefintions.js
+	// Try not to exceed 4 paragraphs
 	// author.(question Number)(author number)
-	vm.question1  = student.question; 	 	vm.summary1   = student.psummary;
-	vm.authors11  = student.pauthor[0];		vm.authors12  = student.pauthor[1];		vm.authors13  = student.pauthor[2];
-
-	vm.question2  = devin.question;		 	vm.summary2   = devin.psummary;
+	// -----------------------------
+	vm.question1  = student.question;
+	vm.authors11  = student.pauthor[0];		vm.authors12  = student.pauthor[1];		
+	vm.authors13  = student.pauthor[2];
+	vm.sum1para1  = student.psum1;			vm.sum1para2  = student.psum2;			
+	vm.sum1para3  = student.psum3;			vm.sum1para4  = student.psum4;
+	// -----------------------------
+	vm.question2  = devin.question;
 	vm.authors21  = devin.pauthor[0];		vm.authors22  = devin.pauthor[1];
-
-	vm.question3  = eburdon.question;		vm.summary3   = eburdon.psummary;
+	vm.sum2para1  = devin.psum1;			vm.sum2para2  = devin.psum2;			
+	vm.sum2para3  = devin.psum3;			vm.sum2para4  = devin.psum4;
+	// -----------------------------
+	vm.question3  = eburdon.question;
 	vm.authors31  = eburdon.pauthor[0];
-
-	vm.question4  = gregnr.question;		vm.summary4   = gregnr.psummary;
-	vm.authors41  = gregnr.pauthor[0];		vm.authors42  = gregnr.pauthor[1];		vm.authors43  = gregnr.pauthor[2];		vm.authors44  = gregnr.pauthor[3];
-
-	vm.question5  = guand.question;			vm.summary5   = guand.psummary;
-	vm.authors51  = guand.pauthor[0];		vm.authors52  = guand.pauthor[1];		vm.authors53  = guand.pauthor[2];
-
-	vm.question6  = Hoverbear.question;		vm.summary6   = Hoverbear.psummary;
+	vm.sum3para1  = eburdon.psum1;			vm.sum3para2  = eburdon.psum2;			
+	vm.sum3para3  = eburdon.psum3;			vm.sum3para4  = eburdon.psum4;
+	// -----------------------------
+	vm.question4  = gregnr.question;
+	vm.authors41  = gregnr.pauthor[0];		vm.authors42  = gregnr.pauthor[1];		
+	vm.authors43  = gregnr.pauthor[2];		vm.authors44  = gregnr.pauthor[3];
+	vm.sum4para1  = gregnr.psum1;			vm.sum4para2  = gregnr.psum2;			
+	vm.sum4para3  = gregnr.psum3;			vm.sum4para4  = gregnr.psum4;
+	// -----------------------------
+	vm.question5  = guand.question;
+	vm.authors51  = guand.pauthor[0];		vm.authors52  = guand.pauthor[1];		
+	vm.authors53  = guand.pauthor[2];
+	vm.sum5para1  = guand.psum1;			vm.sum5para2  = guand.psum2;			
+	vm.sum5para3  = guand.psum3;			vm.sum5para4  = guand.psum4;
+	// -----------------------------
+	vm.question6  = Hoverbear.question;
 	vm.authors61  = Hoverbear.pauthor[0];	vm.authors62  = Hoverbear.pauthor[1];
-
-	vm.question7  = Jsryo.question;			vm.summary7   = Jsryo.psummary;
-	vm.authors71  = Jsryo.pauthor[0];		vm.authors72  = Jsryo.pauthor[1];		vm.authors73  = Jsryo.pauthor[2];
-
-	vm.question8  = knowelsc.question;		vm.summary8   = knowelsc.psummary;
+	vm.sum6para1  = Hoverbear.psum1;		vm.sum6para2  = Hoverbear.psum2;			
+	vm.sum6para3  = Hoverbear.psum3;		vm.sum6para4  = Hoverbear.psum4;
+	// -----------------------------
+	vm.question7  = Jsryo.question;
+	vm.authors71  = Jsryo.pauthor[0];		vm.authors72  = Jsryo.pauthor[1];		
+	vm.authors73  = Jsryo.pauthor[2];
+	vm.sum7para1  = Jsryo.psum1;			vm.sum7para2  = Jsryo.psum2;			
+	vm.sum7para3  = Jsryo.psum3;			vm.sum7para4  = Jsryo.psum4;
+	// -----------------------------
+	vm.question8  = knowelsc.question;
 	vm.authors81  = knowelsc.pauthor[0];	vm.authors82  = knowelsc.pauthor[1];
-
-	vm.question9  = mrivettuvic.question;	vm.summary9   = mrivettuvic.psummary;
-	vm.authors91  = mrivettuvic.pauthor[0];	vm.authors92  = mrivettuvic.pauthor[1];
-
-	vm.question10 = PolloDiablo.question;	vm.summary10  = PolloDiablo.psummary;
+	vm.sum8para1  = knowelsc.psum1;			vm.sum8para2  = knowelsc.psum2;			
+	vm.sum8para3  = knowelsc.psum3;			vm.sum8para4  = knowelsc.psum4;
+	// -----------------------------
+	vm.question9  = mrivettuvic.question;
+	vm.authors91  = mrivettuvic.pauthor[0];		vm.authors92  = mrivettuvic.pauthor[1];
+	vm.sum9para1  = mrivettuvic.psum1;			vm.sum9para2  = mrivettuvic.psum2;			
+	vm.sum9para3  = mrivettuvic.psum3;			vm.sum9para4  = mrivettuvic.psum4;
+	// -----------------------------
+	vm.question10 = PolloDiablo.question;
 	vm.authors101 = PolloDiablo.pauthor[0];
+	vm.sum10para1 = PolloDiablo.psum1;			vm.sum10para2 = PolloDiablo.psum2;			
+	vm.sum10para3 = PolloDiablo.psum3;			vm.sum10para4 = PolloDiablo.psum4;
 
 })
 
--- webApp/public/assets/js/TextContent/projects.js
+++ webApp/public/assets/js/TextContent/projects.js
@@ -20,79 +20,99 @@ var student = {
 	pauthor: 	["@371student", "@chrisjcook", "@sarahannnicholson"],
 	path: 		"assets/project_sources/371student/",
 	question: 	"How is the growth of a software project affected by coupling?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes three codebases: Django, Pyramid, and Flask.",
+	psum2: 		"Results from this tool could be used to make insights about Python software projects that use Git as a versioning tool. It could potentially draw attention to things like technical debt / legacy code. Insights on the development process itself can be made by knowing how adding dependencies (or increasing coupling) affects the growth of the project.", 
+	psum3: 		"This tool first executes a script that moves iteratively throught the history of the master branch of a software project, then uses Snakefood (http://furius.ca/snakefood/) to capture the complexity of the dependency graph at that point in time. This complexity metric (\"Coupling Factor\") is calculated as the ratio of dependency-graph edges to nodes.",
+	psum4: 		"It then uses GitStats (https://github.com/hoxu/gitstats) to serve monthly-growth information. This growth metric (\"Growth Factor\") is calculated by: 1) Dividing average commits/month by average Coupling Factor, 2) Multiply by 4, 3) Commits/Month divided by number determied in step 2.", 
 };
 
 var devin = {
 	pauthor: 	["@devin13", "@DigitalCoffee"],
 	path: 		"assets/project_sources/371student/devin13/",
 	question: 	"How does the number of progressive (new feature) and anti-regressive (maintenance) changes over time affect the number of bugs discovered?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY ",
-	runcmd: 	["ONE", "STEP TWO"]
+	psum1: 		"This tool analyzes three codebases: Ruby on Rails, node.js, Bootstrap.",
+	psum2: 		"This is useful for showing the usefulness (or not) of anti-regressive changes such as refactoring code, and if too many new features are causing unreasonable spikes in bugs.", 
+	psum3: 		"This tool gathers data to create graphs of progressive changes, anti-regressive changes and the number of reported bugs. The data on bugs, anti-regressive and progressive changes will be taken by performing searches in the GitHub issue tracker, which can be searched using \"bug created:2012-11-01..2012-12-01\" to specify the dates of tickets created with the word bug in them, or \"label:bug created:2012-11-01..2012-12-01\" if that project uses a label to represent bugs. The tool uses a Python script that hits GitHub's API in monthly increments to search the number of issues or pull requests with certain queries and/or labels.",
+	psum4: 		"The script will print out the number of found issues as it receives them, about every 3 seconds (to keep within github's rate limit), and once it reaches the end date, will print out the total, and the month number/number of found issues in csv format.", 
 };
 
 var eburdon = {
 	pauthor: 	["@eburdon"],
 	path: 		"assets/project_sources/eburdon/",
 	question: 	"How has Python become more powerful over time?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes just one codebase, Python.",
+	psum2: 		"Instead of looking at an industry project based on a language or framework, this tool attemps to map the evolution of Python itself. It looks at the three major pieces of Python: It's codebase (source code and modules), the changes in types of projects written with Python over time, and its user base or development community.", 
+	psum3: 		"This tool is heavy in written data and screecaptures, not an automatic scripted code, so it's suggested to read the data provided in the repository at https://github.com/eburdon.",
+	psum4: 		"", 
 };
 
 var gregnr = {
 	pauthor: 	["@gregnr", "@jordan-heemskerk", "@rsaujla", "@pcatkins"],
 	path: 		"assets/project_sources/gregnr/",
 	question: 	"Does the volume of unit tests in a project relate to the frequency of bugs?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes three codebases: Pixi.js, Angular.js, and MelonJS",
+	psum2: 		"It attempts to show that the amount of bugs a project has will decrease as the volume of unit tests increase. It could be of great interest to developers as understanding a relationship between test cases and issues would help to allocate the proper amount of resources to QA.",
+	psum3: 		"The developers have identified code bases that have a significant history of unit tests and provide issue tracking on GitHub.",
+	psum4: 		"It executes a script that, given a github repository and a path to the unit test folder, will determine two data sets: frequency of bugs, and volume of unit tests over time. It gathers the frequency of bugs by recording the number of issues opened per unit time. It then gathers the volume of unit tests by measuring the number of lines of code in all files within the test directory per unit time. The unit of time will be weekly. Finally, it graphs the data to see if there's any relationships.",
 };
 
 var guand = {
 	pauthor: 	["@guand", "@paulmoon", "@Iamj1234"],
 	path: 		"assets/project_sources/guand/",
 	question: 	"How does the rate of feature additions change as a project grows in size?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes three codebases: Backbone, Bootstrap, and Homebrew.",
+	psum2: 		"The results from this tool is important because it shows the relationship between the number of features added to a project and the size of the project. Developers may be interested in this relationship to see if they are continuing to add features to improve user satisfaction rather than only fixing bugs.", 
+	psum3: 		"This tool uses GitHub API to collect closed issues with the feature label, and a list of the pull requests merged into master. It then uses Gitstats to get information such as Lines of COde from each repository, converted into YYYY-MM-DD format. Finally, Google Sheets is used to plot the information.",
+	psum4: 		"", 
 };
 
 var Hoverbear = {
 	pauthor: 	["@Hoverbear", "@fraserd"],
 	path: 		"assets/project_sources/Hoverbear/",
 	question: 	"Can we track when different parts of codebases undergo movement during refactoring?",
-	psummary: 	"As developers, we commonly restructure our code. This is usually done in one commit (otherwise it's sloppy). ..."
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes four codebases: capnproto-rust, rust-url, git2-rs, connect.",
+	psum2: 		"As developers, we commonly restructure our code. This is usually done in one commit (otherwise it's sloppy). This tool does not perform significant gathering of outside metrics. Instead, the developers generated several test repositories which were then used to verify the functionality of the tool.", 
+	psum3: 		"Overall, it was found that Transit is successful in detecting code moves. Some of the detected moves where not simple refactoring but changes that would have changed the logic of the analyzed programs. It is worth noting that beyond our small test data, the developers did not check the percentage of moves that were not detected by Transit.",
+	psum4: 		"It's recommended that you visit the source repository for more information.", 
 };
-
+///////
 var Jsryo = {
 	pauthor: 	["@Jsryo", "@EvanHildebrandt", "@KRollans"],
 	path: 		"assets/project_sources/Jsryo/",
 	question: 	"Does the file structure of the source code affect the ability for the project to evolve?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes four codebases: JQuery Mobile, JQuery, Bootstrap, and Rails.",
+	psum2: 		"It analyzes releases and tags them based on Security Updates, Bug fixes, feature releases, preformance improvements, and/or refactors. The tool then uses some statistics on these to understand the frequency of these types of updates. Developers and readers can then visually inspect the codebase using dependancy visualization tools such as Gource to gain details of the nature of these changes.", 
+	psum3: 		"",
+	psum4: 		"", 
 };
 
 var knowelsc = {
 	pauthor: 	["@knowelsc", "@ryanmcdonald"],
 	path: 		"assets/project_sources/knowelsc/",
 	question: 	"What can available tools tell us about code quality?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes three codebases: Ruby on Rails, node.js, Bootstrap.",
+	psum2: 		"Actually, we should rephrase. This isn't so much as a tool as a research methodology. It's suggested that you visit the project repository for exact details on the results this development team collected and how you can do it yourself using the tools they found. It's pretty cool though - they were able to assign a GPA to various projects based on the metrics they collected.", 
+	psum3: 		"",
+	psum4: 		"", 
 };
 
 var mrivettuvic = {
 	pauthor: 	["@mrivettuvic", "@MisterPotter"],
 	path: 		"assets/project_sources/mrivettuvic/",
 	question: 	"What is the relation between software development methodologies and development time per feature in a software system?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes four codebases: Marmotta, ProjectForge, SonarQube, and DSpace.",
+	psum2: 		"It gathers a series of metrics through the issue tracking software that is used by the selected codebases (Jira) via a Python script that assumes the same issue tracking software had been used throughout the lifespan of the project. The starting and finishing date will be used to calculate the time spent on that feature, and the date they started using the issue tracking software will be used so the time they have completed features relative to the time in to the project can be measured.", 
+	psum3: 		"To control the results, collected feature information will filter outliers by collecting 'major' priority features that were completed in over 30 minutes, and less than one year. For every collected codebase, you will have to input what software development method they used manually. When the information is collected, an appropriate graph will be created using matplotlib.",
+	psum4: 		"", 
 };
 
 var PolloDiablo = {
 	pauthor: 	["@PolloDiablo"],
 	path: 		"assets/project_sources/PolloDiablo/",
 	question: 	"Do developers respond to user feedback submitted through online forums?",
-	psummary: 	"SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY SUMMARY "
-	// runcmd: 	["ONE", "STEP TWO", ... ]
+	psum1: 		"This tool analyzes just one codebase: Reddit. More specifically, it looks at the patch notes and subreddits for League of Legends, World of Warcraft, and Team Fortress 2.",
+	psum2: 		"This tool is entirely written in Java. It has one file with all the code necessary to scrape data from Reddit Posts and store it in the RedditPosts database table. With this the user is able to create an interface to use the information. The next file contains all the code necessary to scrape data from Patch Notes and store it in the PatchNotes database table, which the user can then use to implement the next interface. Finally, with the first two database tables created, a third analysis file can be used to generate .csv files which can then generate graphs in Microsoft Excel.", 
+	psum3: 		"",
+	psum4: 		"", 
 };
 
